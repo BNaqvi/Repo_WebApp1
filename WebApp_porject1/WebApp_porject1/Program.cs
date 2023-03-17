65c25a4880ce52615e8b66a2f3506092bc98ce32
BasharatNaqvi <boxofsms@gmail.com>
2023-03-17 20:06:52 +0500

    updation

--- WebApp_porject1/WebApp_porject1/Program.cs
+++ WebApp_porject1/WebApp_porject1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.  // Add services to the container. // Add services to the container.
+// Add services to the container.  // Add services to the container. (duplicated locally to push on git)
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
